9f5334b70354b5097cb09b3d4d2523c0b3524987
REAM.KHORN <REAM.KHORN@PNC.LOCAL>
2026-07-09 08:58:13 +0700

    update more feature

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\ClassController;
 use App\Http\Controllers\Api\StudentController;
+use App\Http\Controllers\Api\SubjectController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -11,6 +12,17 @@ Route::post('/login', [AuthController::class, 'login']);
 Route::middleware('auth:sanctum')->group(function () {
     Route::post('/logout', [AuthController::class, 'logout']);
     Route::get('/user', [AuthController::class, 'user']);
+
+    // Student routes
+    Route::get('/students', [StudentController::class, 'index']);
+    Route::post('/students', [StudentController::class, 'store']);
+    Route::get('/students/{student}', [StudentController::class, 'show']);
+    Route::put('/students/{student}', [StudentController::class, 'update']);
+    Route::delete('/students/{student}', [StudentController::class, 'destroy']);
+    Route::put('/students/{student}/assign-class', [StudentController::class, 'assignClass']);
+
+    // Class routes
+    Route::get('/classes/list', [ClassController::class, 'list']);
 
     // Subject routes
     Route::get('/subjects', [SubjectController::class, 'index']);
